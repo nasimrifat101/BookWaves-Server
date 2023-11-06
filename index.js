74bbdb57aedb8bc9552ddc5d0fcb41365cb07030
nasimrifat101 <nasimrifat101@gmail.com>
2023-11-07 01:06:30 +0600

    jwt added

--- index.js
+++ index.js
@@ -1,12 +1,20 @@
 const express = require("express");
 const cors = require("cors");
+const jwt = require(`jsonwebtoken`);
+const cookieParser = require("cookie-parser");
 const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 require("dotenv").config();
 const app = express();
 const port = process.env.PORT || 5000;
 
-app.use(cors());
+app.use(
+  cors({
+    origin: [`http://localhost:5173`],
+    credentials: true,
+  })
+);
 app.use(express.json());
+app.use(cookieParser());
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.yuxyuxp.mongodb.net/?retryWrites=true&w=majority`;
 
@@ -19,6 +27,27 @@ const client = new MongoClient(uri, {
   },
 });
 
+// middlewares
+const logger = (req, res, next) => {
+  console.log("log: info", req.method, req.url);
+  next();
+};
+
+const verifyToken = (req, res, next) => {
+  const token = req.cookies?.token;
+  // console.log(`token in the middleware`, token);
+  if (!token) {
+    return res.status(401).send({ message: "Unauthorized access" });
+  }
+  jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (error, decoded) => {
+    if (error) {
+      return res.status(401).send({ message: "unauthorized access" });
+    }
+    req.user = decoded;
+    next();
+  });
+};
+
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
@@ -27,6 +56,28 @@ async function run() {
     const booksCollection = client.db("BookWaves").collection("books");
     const categoryCollection = client.db("BookWaves").collection("brands");
     const borrowCollection = client.db("BookWaves").collection("borrows");
+
+    // auth api
+    app.post("/jwt", async (req, res) => {
+      const user = req.body;
+      console.log("token for user", user);
+      const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {
+        expiresIn: "2h",
+      });
+
+      res.cookie("token", token, {
+        httpOnly: true,
+        secure: true,
+        sameSite: "none",
+      });
+      res.send({ success: true });
+    });
+
+    app.post("/logout", async (req, res) => {
+      const user = req.body;
+      console.log("loggingout user", user);
+      res.clearCookie("token", { maxAge: 0 }).send({ success: true });
+    });
 
     // rendering home card
     app.get("/brands", async (req, res) => {
@@ -132,8 +183,12 @@ async function run() {
       res.send(result);
     });
     // api for email fetch
-    app.get("/borrowing", async (req, res) => {
-      // console.log(req.query.email);
+    app.get("/borrowing", logger, verifyToken, async (req, res) => {
+      console.log(req.query.email);
+      console.log("token owner", req.user);
+      if(req.user.email !== req.query.email){
+        return res.status(403).send({message: 'Forbidden access'})
+      }
       let query = {};
       if (req.query?.email) {
         query = { email: req.query.email };
@@ -164,9 +219,6 @@ async function run() {
 
       res.send(result);
     });
-
-
-    
 
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
